3fded9bcd9e306edbf191a318f2fcdeca25fd4f1
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-08-10 18:40:25 -0600

    Clean files

--- page.php
+++ page.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Restrict_Content_Posts
- */
 
 get_header();
 ?>
@@ -23,15 +11,14 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
-		endwhile; // End of the loop.
+		endwhile;
 		?>
 
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_sidebar();
